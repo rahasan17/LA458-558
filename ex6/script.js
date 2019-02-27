451eb5365286261f9610ab00109675a13cb3e64f
Rakibul Ahasan <rakibulahasan@gmail.com>
2019-02-26 21:28:01 -0600

    Update script.js

--- ex6/script.js
+++ ex6/script.js
@@ -2,9 +2,6 @@ var map = L.map('map', {minZoom:15}).setView(([42.041484, -93.640635], 16);
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
-    L.marker([42.041484, -93.640635]).addTo(map)
-        .bindPopup("Home")
-        .openPopup();
 
 var coordinates = [
     [42.041484, -93.640635],
