e1f5156dfbd98a3c2de68147f450bbe6a25fd769
Rakibul Ahasan <rakibulahasan@gmail.com>
2019-03-05 01:01:00 -0600

    Update script.js

--- Assignment2/script.js
+++ Assignment2/script.js
@@ -1,4 +1,4 @@
-
+//defining the map extent
 var map = L.map('map').setView([22.80355, 89.53242], 16);
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -26,29 +26,29 @@ map.on('click',onMapClick);
 $(document).ready(function(){
 
 $('#Dhaka').click(function() {
-map.setView([23.814854, 90.411102],10 );
+map.setView([23.814854, 90.411102],10 ); //this is the extent to the capital of Bangladesh [Dhaka city]
 });
 var marker4 = L.marker([23.814854, 90.411102], {
     draggable: true,
     opacity:1
-}).addTo(map).bindPopup('<a href="https://en.wikipedia.org/wiki/Dhaka"> DHAKA CITY </a>').openPopup();
+}).addTo(map).bindPopup('<a href="https://en.wikipedia.org/wiki/Dhaka"> DHAKA CITY </a>').openPopup(); //Link to the wiki page of the City of Dhaka
 $('#Khulna').click(function() {
-map.setView([22.803558, 89.532428],13 );
+map.setView([22.803558, 89.532428],13 ); //the view is set to highlight my undergrad institution
 var marker2 = L.marker([22.803558, 89.532428]).addTo(map)
-        marker2.bindPopup('<a href="http://ku.ac.bd//" target="_blank"> Khulna University </a>').openPopup();
+        marker2.bindPopup('<a href="http://ku.ac.bd//" target="_blank"> Khulna University </a>').openPopup(); //link to the webpage of the university
 var marker3 = L.marker([22.803607, 89.534197], {
     draggable: true,
     opacity: 0
 }).addTo(map).bindPopup("Let's find something about my Undergrad University by clicking on the marker").openPopup();
 });
 
-//adding few important location points of my undergrad life and google map markers to show them
+//adding few important locations where i most frequently visit when i was a student
 var myPoints = [
     [22.802731, 89.534758, "set", "Academic Building 1"],
     [22.802276, 89.537611, "dorm", "Khan Bahadur Ahsanullal Hall"],
     [22.801845, 89.534559, "cafe", "Central Cafe"]
     ];
-
+//using googlemapmarkers to show the most frequently visited locations
 function getColor(s) {
     return  s == "set" ? 'http://www.googlemapsmarkers.com/v1/e41a1c' :
             s == "dorm" ? 'http://www.googlemapsmarkers.com/v1/377eb8' :
