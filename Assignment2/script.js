88c766e3448e2e883e56660336ef048dfcc894a0
Rakibul Ahasan <rakibulahasan@gmail.com>
2019-03-04 22:05:51 -0600

    Update script.js

--- Assignment2/script.js
+++ Assignment2/script.js
@@ -28,23 +28,26 @@ $(document).ready(function(){
 $('#Dhaka').click(function() {
 map.setView([23.817682, 90.411790],10 );
 });
-
-$('#Khulna').click(function() {
-map.setView([22.803558, 89.532428],13 );
-var marker2 = L.marker([22.803558, 89.532428]).addTo(map)
-        marker2.bindPopup('<a href="http://ku.ac.bd//" target="_blank"> Khulna University </a>').openPopup();
-
-  var marker3 = L.marker([23.817682, 90.411790], {
+var marker4 = L.marker([23.817682, 90.411790], {
     draggable: true,
     opacity:1
 }).addTo(map).bindPopup('<a href="https://en.wikipedia.org/wiki/Dhaka"> DHAKA CITY </a>').openPopup();
 
-var marker3 = L.marker([23.817682, 90.411790], {
+var marker5 = L.marker([23.817682, 90.411790], {
     draggable: true,
     opacity: 0
-}).addTo(map).bindPopup("By CLickikng the links on the beside markers you will find information on my Undergrad University and the city i Live in ").openPopup();
+}).addTo(map).bindPopup("By CLickikng the link on the beside marker you will find information on the city i Live in ").openPopup();
 });
+$('#Khulna').click(function() {
+map.setView([22.803558, 89.532428],13 );
+var marker2 = L.marker([22.803558, 89.532428]).addTo(map)
+        marker2.bindPopup('<a href="http://ku.ac.bd//" target="_blank"> Khulna University </a>').openPopup();
+var marker3 = L.marker([22.803607, 89.534197], {
+    draggable: true,
+    opacity: 0
+}).addTo(map).bindPopup("By CLickikng the links on the beside marker you will find information on my Undergrad University").openPopup();
 });
+
 
 $('#getzoom').click(function() {
 
