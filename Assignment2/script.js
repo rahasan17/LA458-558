7cc6bb87754aca38d34664272f28305f87ef23fb
Rakibul Ahasan <rakibulahasan@gmail.com>
2019-03-04 22:28:34 -0600

    Update script.js

--- Assignment2/script.js
+++ Assignment2/script.js
@@ -1,6 +1,6 @@
 
 var marker;
-var map = L.map('map', {minZoom:16}).setView([22.803558, 89.532428], 17);
+var map = L.map('map', {minZoom:15}).setView([22.803558, 89.532428], 16);
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
