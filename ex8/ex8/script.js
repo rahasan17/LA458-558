dba002cbf059c0183f232704951e647ef77ada16
Rakibul Ahasan <rakibulahasan@gmail.com>
2019-03-14 22:58:32 -0500

    Update script.js

--- ex8/ex8/script.js
+++ ex8/ex8/script.js
@@ -47,14 +47,14 @@ var geojsonLayer = new L.GeoJSON.AJAX(myURL , {
     }
 }).addTo(map);
 
-var legend = L.control({position: 'bottomleft'});
+var legend = L.control({position: 'bottomright'});
 legend.onAdd = function (map) {
     var div = L.DomUtil.create('div', 'info legend');
     // loop through items and generate legend items each
-    for (var i = 0; i < feature.properties.type.length; i++) {
+    for (var i = 0; i < feature.properties.rank.length; i++) {
         div.innerHTML +=
-            '<i style="background:' + getColor(feature.properties.type[i]) + ' "></i> ' +
-            labels[i] + (feature.properties.type ? '' +  '<br>' : '');
+            '<i style="background:' + getColor(feature.properties.rank[i]) + ' "></i> ' +
+            labels[i] + (feature.properties.rank ? '' +  '<br>' : '');
     }
     return div;
 };
